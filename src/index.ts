f81b8c67608e1d626a132b9f65b96108d4374c8c
Leo Wu <leo.wu.kw@gmail.com>
2025-04-21 23:34:52 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import { createClient } from "redis";
+import {createClient} from "redis";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const redisClient = createClient({ url: process.env.REDIS_URL });
+const redisClient = createClient({url: process.env.REDIS_URL});
 
 redisClient.connect().catch(console.error);
+
+const PLACES = 5;
 
 app.get("/:key/count.svg", async (req, res) => {
     const key = req.params.key;
@@ -16,14 +18,24 @@ app.get("/:key/count.svg", async (req, res) => {
     // Increment the counter
     const count = await redisClient.incr(`counter:${key}`);
 
-    // Create SVG
-    const svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="180" height="20">
-      <rect width="100%" height="100%" fill="#555"/>
-      <text x="90" y="14" fill="#fff" font-family="Verdana" font-size="11" text-anchor="middle">
-        ${key}: ${count}
-      </text>
-    </svg>
+    const countArray = count.toString().padStart(PLACES, "0").split("");
+    const parts = countArray.reduce(
+        (acc, next, index) =>
+            `${acc}
+       <rect id="Rectangle" fill="#000000" x="${index * 32}" width="29" height="29"></rect>
+       <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill="#00FF13">
+           <tspan x="${index * 32 + 7}" y="22">${next}</tspan>
+       </text>
+`,
+        ""
+    );
+    const svg = `<?xml version="1.0" encoding="UTF-8"?>
+  <svg width="${PLACES * 32}px" height="30px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>Count</title>
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        ${parts}
+      </g>
+  </svg>
   `;
 
     res.setHeader("Content-Type", "image/svg+xml");
